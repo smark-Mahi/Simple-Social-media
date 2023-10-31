a4b6470a96f070cd263e6c8a3ddd900d015db718
kmzee-ark <arkhan@kmzee.online>
2023-10-31 09:07:52 +0530

    websocket fixed

--- src/helpers/socket.ts
+++ src/helpers/socket.ts
@@ -4,10 +4,7 @@ import { io } from "socket.io-client";
 export const socket = io("wss://simple-social.onrender.com",
 {
         transports:["websocket"],
-        // addTrailingSlash:false,
     path:"/ws/socket.io/",
-//     reconnectionAttempts:0,
-    // requestTimeout:100000,
-    // reconnectionDelay: 100000
+
 }
 );
