d4ec40c8416a45d8e5646032c41f9f76b08f34fe
kmzee-sm <133731546+kmzee-sm@users.noreply.github.com>
2024-01-12 12:19:55 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { setUpInterceptors } from "./api/auth.ts";
 import SignUPInfoContextProvider from "./Context/SignUPInfoContext.tsx";
-import { createTheme } from "@mui/material";
-import { ThemeProvider } from "@emotion/react";
+// import { createTheme } from "@mui/material";
+// import { ThemeProvider } from "@emotion/react";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en.json";
 import ru from "javascript-time-ago/locale/ru.json";
@@ -26,14 +26,14 @@ setUpInterceptors();
 TimeAgo.addDefaultLocale(en);
 TimeAgo.addLocale(ru);
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      light: "#C0C2C9",
-      main: "#708090",
-    },
-  },
-});
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       light: "#C0C2C9",
+//       main: "#708090",
+//     },
+//   },
+// });
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
